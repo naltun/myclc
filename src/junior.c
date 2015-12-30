237363f58823cc9a643895095793ca72bcf73149
Noah <nba4191@gmail.com>
2015-12-30 13:55:52 +0000

    Add language parser

--- src/junior.c
+++ src/junior.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "../libs/mpc.h"
 
-/* Compiles these functions if compiling on a Windows */
+/* Compile these functions if compiling on a Windows */
 #ifdef _WIN32
 #include <string.h>
 
@@ -28,6 +27,22 @@ void add_history(char*, unused) {}
 
 int main(int argc, char** argv) {
     
+    /* syntax parsers */
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Junior   = mpc_new("junior");
+    
+    /* Junior language definition */
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                      \
+        number   : /-?[0-9]+/ ;                            \
+        operator : '+' | '-' | '*' | '/' ;                 \
+        expr     : <number> | '(' <operator> <expr>+ ')' ; \
+        junior   : /^/ <operator> <expr>+ /$/ ;            \
+    ",
+    Number, Operator, Expr, Junior);
+    
     puts("\n\tJunior\nDeveloped by Noah Altunian (github.com/naltun/)\n");
     puts("Press ctrl+C to Exit\n");
     
@@ -36,16 +51,23 @@ int main(int argc, char** argv) {
         /* Output prompt and retrieve input */
         char* input = readline(">> ");
         
-        /* Add input to history (for retrieval with up/down arrows) */
-        add_history(input);
+        /* Parse user input */
+        mpc_result_t r;
+        if (mpc_parse("<stdin", input, Junior, &r)) {
+            /* If parse is successful, print and delete AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            /* If parse is not successful, print and delete Error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
         
-        /* Return input */
-        printf("%s\n", input);
-        
-        /* Free input memory */
         free(input);
-        
     }
+    
+    /* Undefine and delete parsers */
+    mpc_cleanup(4, Number, Operator, Expr, Junior);
     
     return 0;
 }
