f33f1d541fe0eacb512eb5aa34c5c945e47fce26
Noah <nbaltunian@gmail.com>
2018-09-22 12:32:31 -0400

    Add blank space

--- src/myclc.c
+++ src/myclc.c
@@ -329,7 +329,7 @@ int main(int argc, char** argv) {
         char* input = readline(">> ");
         
         // if input is 'exit' or 'quit', then exit with a status of 0
-        if (strcmp(input, "exit") == 0 || strcmp(input, "quit") ==0) { exit(0); }
+        if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) { exit(0); }
 
         // add user input to input history
         add_history(input);
